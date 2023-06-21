5369f680485241a077da5ef0d03d86efb93785f8
Pat <patbelcher@yahoo.com>
2023-06-21 12:04:57 -0400

    first commit mysql read db using postman wed june 21

---
+++ src/movies.js
@@ -0,0 +1,11 @@
+import { db } from  './connectDb.js'
+
+export function getMovies(req, res) {
+    db.query('select * from movies', (err, movies) => {
+        if(err) {
+            res.status(500).send({success: false, message: err})
+            return
+        }
+        res.send(movies)
+    } )
+}
